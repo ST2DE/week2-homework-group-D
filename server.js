7e382b2868c0f840e589a911f3d43074b609d406
DyslexiaS <exist02593@gmail.com>
2018-04-06 17:52:02 +0800

    main page header

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+app.listen(3000);
+const path = require('path');
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/' + 'main.html');
+});
+
+app.listen(3000, function(){
+    console.log('App is running on port 3000.');
+});
